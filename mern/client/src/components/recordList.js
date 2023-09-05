fe5749366a8f59b065b2bc7ccc8da6bd505354cb
myles <mrmylesmc@gmail.com>
2023-09-04 19:20:34 -0500

    forking to make amern map using maplibre

--- mern/client/src/components/recordList.js
+++ mern/client/src/components/recordList.js
@@ -2,84 +2,92 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Record = (props) => (
- <tr>
-   <td>{props.record.name}</td>
-   <td>{props.record.position}</td>
-   <td>{props.record.level}</td>
-   <td>
-     <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Edit</Link> |
-     <button className="btn btn-link"
-       onClick={() => {
-         props.deleteRecord(props.record._id);
-       }}
-     >
-       Delete
-     </button>
-   </td>
- </tr>
+  <tr>
+    <td>{props.record.name}</td>
+    <td>{props.record.position}</td>
+    <td>{props.record.level}</td>
+    <td>
+      <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Edit</Link> |
+      <button className="btn btn-link"
+        onClick={() => {
+          props.deleteRecord(props.record._id);
+        }}
+      >
+        Delete
+      </button>
+    </td>
+  </tr>
 );
 
 export default function RecordList() {
- const [records, setRecords] = useState([]);
+  const [records, setRecords] = useState([]);
 
- // This method fetches the records from the database.
- useEffect(() => {
-   async function getRecords() {
-     const response = await fetch(`http://localhost:5050/record/`);
+  // This method fetches the records from the database.
+  useEffect(() => {
+    async function getRecords() {
+      const response = await fetch(`http://localhost:6060/record/` //, {
+        //   method: "GET",
+        //   headers: {
+        //     'Access-Control-Allow-Origin': '*',
+        //   },
+        //   mode: "no-cors"
+        // }
+      );
 
-     if (!response.ok) {
-       const message = `An error occurred: ${response.statusText}`;
-       window.alert(message);
-       return;
-     }
 
-     const records = await response.json();
-     setRecords(records);
-   }
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
 
-   getRecords();
+      const records = await response.json();
+      setRecords(records);
+    }
 
-   return;
- }, [records.length]);
+    getRecords();
 
- // This method will delete a record
- async function deleteRecord(id) {
-   await fetch(`http://localhost:5050/record/${id}`, {
-     method: "DELETE"
-   });
+    return;
+  }, [records.length]);
 
-   const newRecords = records.filter((el) => el._id !== id);
-   setRecords(newRecords);
- }
+  // This method will delete a record
+  async function deleteRecord(id) {
+    await fetch(`http://localhost:6060/record/${id}`, {
+      method: "DELETE"
+    });
 
- // This method will map out the records on the table
- function recordList() {
-   return records.map((record) => {
-     return (
-       <Record
-         record={record}
-         deleteRecord={() => deleteRecord(record._id)}
-         key={record._id}
-       />
-     );
-   });
- }
+    const newRecords = records.filter((el) => el._id !== id);
+    setRecords(newRecords);
+  }
 
- // This following section will display the table with the records of individuals.
- return (
-   <div>
-     <h3>Record List</h3>
-     <table className="table table-striped" style={{ marginTop: 20 }}>
-       <thead>
-         <tr>
-           <th>Name</th>
-           <th>Position</th>
-           <th>Level</th>
-           <th>Action</th>
-         </tr>
-       </thead>
-       <tbody>{recordList()}</tbody>
-     </table>
-   </div>
- );
+  // This method will map out the records on the table
+  function recordList() {
+    return records.map((record) => {
+      return (
+        <Record
+          record={record}
+          deleteRecord={() => deleteRecord(record._id)}
+          key={record._id}
+        />
+      );
+    });
+  }
+
+  // This following section will display the table with the records of individuals.
+  return (
+    <div>
+      <h3>Record List</h3>
+      <table className="table table-striped" style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Level</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{recordList()}</tbody>
+      </table>
+    </div>
+  );
 }
--- mern/server/routes/record.mjs
+++ mern/server/routes/record.mjs
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
 // This section will help you get a single record by id
 router.get("/:id", async (req, res) => {
   let collection = await db.collection("records");
-  let query = {_id: new ObjectId(req.params.id)};
+  let query = { _id: new ObjectId(req.params.id) };
   let result = await collection.findOne(query);
 
   if (!result) res.send("Not found").status(404);
@@ -36,7 +36,7 @@ router.post("/", async (req, res) => {
 // This section will help you update a record by id.
 router.patch("/:id", async (req, res) => {
   const query = { _id: new ObjectId(req.params.id) };
-  const updates =  {
+  const updates = {
     $set: {
       name: req.body.name,
       position: req.body.position,
